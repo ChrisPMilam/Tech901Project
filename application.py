e328792ea5368328eb78e97fc44e343a3bcd3d9e
Levi Coon <lmcoon4@gmail.com>
2018-04-18 01:46:19 +0000

    moved route into application.py

--- application.py
+++ application.py
@@ -79,6 +79,14 @@ def login():
         return render_template("login.html")
 
 
+@app.route("/sqlist")
+def index():
+    """Show list of questions"""
+    questions = db.execute("SELECT * FROM Question WHERE User_ID = :userid", userid=session["user_id"])
+
+    return render_template("sQuestions.html", questions=questions)
+
+
 @app.route("/logout")
 def logout():
     """Log user out"""
--- sQuestions.py
+++
@@ -1,8 +0,0 @@
-@app.route("/sqlist")
-def index():
-    """Show list of questions"""
-    questions = db.execute("SELECT * FROM Question WHERE User_ID = :userid", userid=session["user_id"])
-
-    return render_template("sQuestions.html", questions=questions)
-
-
